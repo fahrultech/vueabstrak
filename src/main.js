3a5d8aaab0670dff58ac39c5eb4dab9f32cad564
Fahrul Rizal <fahrultech@gmail.com>
2020-09-28 08:27:36 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
+import './plugins/axios'
 import App from './App.vue'
+import store from './store'
+import router from './router'
+import SlideUpDown from 'vue-slide-up-down';
+import VueSlimScroll from 'vue-slimscroll'
+
+window.$ = window.jQuery = require('jquery');
+import { BootstrapVue} from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+
+Vue.use(VueSlimScroll);
+
+Vue.component('slide-up-down', SlideUpDown);
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  router,
+  render: h => h(App)
 }).$mount('#app')
